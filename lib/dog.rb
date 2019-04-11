4eb2053570aa88fbb626e9520e5340238cbc0b09
Andy Feidje Joseph <ingandy75@gmail.com>
2019-04-11 14:52:47 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,6 @@
+class Dog
+    def initialize(input_name,input_breed = "Mutt")
+      @name = input_name
+      @breed = input_breed
+    end
+end
---
+++ lib/person.rb
@@ -0,0 +1,7 @@
+class Person
+
+    def initialize(input_name)
+      @name = input_name
+    end
+
+end
